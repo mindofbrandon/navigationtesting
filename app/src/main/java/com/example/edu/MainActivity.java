4162f2282d6bf89a8c0072662ac88d7895daead8
Brandon Zuniga <brandon_zuniga@student.uml.edu>
2021-12-01 10:25:46 -0500

    removed title bar

--- app/src/main/java/com/example/edu/MainActivity.java
+++ app/src/main/java/com/example/edu/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.edu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
@@ -19,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
         // TODO: have app learn when user is already logged in, vs new user so that it doesnt always jump to sign up page
         setContentView(R.layout.activity_main); // when app launches, display activity_main xml
+
+
 
     }
 
